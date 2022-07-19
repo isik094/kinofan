2f72e063eab0e73f23aa736219a2c5442c3c7693
Renat <renat-911@mail.ru>
2022-07-19 15:50:52 +0000

    Revert "Bc"
    This reverts commit 739dcc231db4198c624bc7dd7f4dd15359adcbc6

--- common/config/main.php
+++ common/config/main.php
@@ -13,10 +13,5 @@ return [
             'class' => \common\components\Dump::class,
             'lifeTime' => 86400 * 30 //один месяц
         ],
-        'bc' => [
-            'class' => \common\components\Bc::class,
-            'address' => 'your address',
-            'keyName' => 'your key name',
-        ],
     ],
 ];
