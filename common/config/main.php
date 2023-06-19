cb505785d09b2e714e6a13dd614b79fd11e6eb55
isik94 <isik94@icloud.com>
2023-06-19 18:46:14 +0300

    feat(auth): set RU language project

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,7 @@ return [
         '@npm'   => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'language' => 'ru-RU',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
