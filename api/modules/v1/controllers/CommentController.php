1d9a15d17d4ebd3ac14b44a0990fa06286bc99d4
isik94 <isik94@icloud.com>
2023-08-10 19:01:48 +0300

    feat(comment): add comment controller

---
+++ api/modules/v1/controllers/CommentController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use Yii;
+use api\components\ApiDataPagination;
+use api\components\ApiResponse;
+use api\components\ApiResponseException;
+use api\modules\v1\models\search\CommentSearch;
+use api\modules\v1\traits\CommentData;
+
+class CommentController extends ApiWithSearchController
+{
+    protected bool $isPrivate = false;
+
+    use CommentData;
+
+    /**
+     * @inheritDoc
+     */
+    public function getSearchModel(): CommentSearch
+    {
+        return new CommentSearch();
+    }
+
+    /**
+     * @brief Получить список комментариев к фильму
+     * @param int|null $page
+     * @param int $limit
+     * @param string|null $sort
+     * @return ApiResponse|ApiResponseException
+     */
+    public function actionIndex(int $page = null, int $limit = 20, string $sort = null): ApiResponse|ApiResponseException
+    {
+        try {
+            $searchModel = $this->getSearchModel();
+            $query = $searchModel->search(Yii::$app->request->get());
+
+            $apiSearch = $this->searchAndSort($page, $limit, $sort, $query);
+
+            $data = $this->makeObjects($apiSearch->query->all(), $this->commentData());
+
+            return new ApiResponse(false, new ApiDataPagination($data, $apiSearch->lastPage));
+        } catch (\Exception $e) {
+            return new ApiResponseException($e);
+        }
+    }
+}
