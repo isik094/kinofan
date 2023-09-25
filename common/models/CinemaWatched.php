74b0b870d1944db2c48be13d7d864cc03300ed3a
isik94 <isik94@icloud.com>
2023-09-25 18:42:46 +0300

    feat(profile): add CinemaWatched model

---
+++ common/models/CinemaWatched.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "cinema_watched".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $cinema_id
+ *
+ * @property Cinema $cinema
+ * @property User $user
+ */
+class CinemaWatched extends \common\base\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'cinema_watched';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'cinema_id'], 'required'],
+            [['user_id', 'cinema_id'], 'integer'],
+            [['cinema_id'], 'exist', 'skipOnError' => true, 'targetClass' => Cinema::className(), 'targetAttribute' => ['cinema_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'cinema_id' => 'Cinema ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[Cinema]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCinema()
+    {
+        return $this->hasOne(Cinema::className(), ['id' => 'cinema_id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
