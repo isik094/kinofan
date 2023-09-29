eab2303be77ce1f204190293fb0d45e4bc55690f
isik94 <isik94@icloud.com>
2023-09-29 18:11:05 +0300

    feat(profile): add personalization form

---
+++ api/modules/v1/models/PersonalizationForm.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace api\modules\v1\models;
+
+use Yii;
+use common\models\UserGenreCinema;
+use common\models\UserHobbies;
+use common\base\Model;
+use common\models\Country;
+use common\models\Genre;
+use common\models\Hobbies;
+use common\models\Profile;
+use common\models\User;
+use common\models\UserCountryCinema;
+
+class PersonalizationForm extends Model
+{
+    /**
+     * @var User
+     */
+    public User $user;
+    /**
+     * @var string
+     */
+    public $sex;
+    /**
+     * @var string
+     */
+    public $birthday;
+    /**
+     * @var int
+     */
+    public $country_id;
+    /**
+     * @var array
+     */
+    public $genre_ids;
+    /**
+     * @var array
+     */
+    public $hobbies_ids;
+
+    public function rules(): array
+    {
+        return [
+            [['sex', 'birthday'], 'string', 'max' => 255],
+            [['country_id'], 'integer'],
+            [['genre_ids', 'hobbies_ids'], 'each', 'rule' => ['integer']],
+            [['genre_ids'], 'genreValidate'],
+            [['hobbies_ids'], 'hobbiesValidate'],
+            ['sex', 'in', 'range' => [Profile::MALE, Profile::FEMALE]],
+            [['country_id'], 'exist', 'skipOnError' => true, 'targetClass' => Country::className(), 'targetAttribute' => ['country_id' => 'id']],
+        ];
+    }
+
+    public function attributeLabels(): array
+    {
+        return [
+            'sex' => 'Пол',
+            'birthday' => 'День рождения',
+            'country_id' => 'ID страны',
+            'genre_ids' => 'ID жанров',
+            'hobbies_ids' => 'ID увлечений',
+        ];
+    }
+
+    /**
+     * @brief Валидация на существование жанра
+     * @param $attribute
+     * @return void
+     */
+    public function genreValidate($attribute): void
+    {
+        $doesNotExist = false;
+        foreach ($this->genre_ids as $genre_id) {
+            if (!Genre::findOne($genre_id)) {
+                $doesNotExist = true;
+            }
+        }
+
+        if ($doesNotExist === true) {
+            $this->addError($attribute, 'В массиве жанры есть не существующая запись');
+        }
+    }
+
+    /**
+     * @brief Валидация на существование увлечения
+     * @param $attribute
+     * @return void
+     */
+    public function hobbiesValidate($attribute): void
+    {
+        $doesNotExist = false;
+        foreach ($this->hobbies_ids as $hobbies_id) {
+            if (!Hobbies::findOne($hobbies_id)) {
+                $doesNotExist = true;
+            }
+        }
+
+        if ($doesNotExist === true) {
+            $this->addError($attribute, 'В массиве жанры есть не существующая запись');
+        }
+    }
+
+    /**
+     * @brief Персонализация
+     * @return User|null
+     * @throws \yii\db\Exception
+     */
+    public function updatePersonalization(): ?User
+    {
+        if (!$this->validate()) {
+            return null;
+        }
+
+        $transaction = Yii::$app->db->beginTransaction();
+        try {
+            $profile = $this->user->profile;
+            $profile->sex = $this->sex;
+            $profile->birthday = strtotime($this->birthday);
+            $profile->saveStrict();
+
+            $this->createUserCountry();
+            $this->createUserGenre();
+            $this->createUserHobbies();
+            $transaction->commit();
+
+            return $this->user;
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * @brief Создать запись о предпочтительной старне кино пользователя
+     * @return bool|null
+     * @throws \Exception
+     */
+    public function createUserCountry(): ?bool
+    {
+        if (
+            !UserCountryCinema::findOne([
+                'user_id' => $this->user->id,
+                'country_id' => $this->country_id
+            ])
+        ) {
+            $model = new UserCountryCinema();
+            $model->user_id = $this->user->id;
+            $model->country_id = $this->country_id;
+
+            return $model->saveStrict();
+        }
+
+        return null;
+    }
+
+    /**
+     * @brief Создать запись о предпочтительном жанре пользователя
+     * @return bool
+     * @throws \Exception
+     */
+    public function createUserGenre(): bool
+    {
+        try {
+            foreach ($this->genre_ids as $genre_id) {
+                if (
+                    UserGenreCinema::findOne([
+                    'user_id' => $this->user->id,
+                    'genre_id' => $genre_id])
+                ) {
+                    continue;
+                }
+
+                $model = new UserGenreCinema();
+                $model->user_id = $this->user->id;
+                $model->genre_id = $genre_id;
+                $model->saveStrict();
+            }
+
+            return true;
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    /**
+     * @brief Создать запись о увлечении пользователя
+     * @return bool
+     * @throws \Exception
+     */
+    public function createUserHobbies(): bool
+    {
+        try {
+            foreach ($this->hobbies_ids as $hobbies_id) {
+                if (
+                    UserHobbies::findOne([
+                    'user_id' => $this->user->id,
+                    'hobbies_id' => $hobbies_id])
+                ) {
+                    continue;
+                }
+                $model = new UserHobbies();
+                $model->user_id = $this->user->id;
+                $model->hobbies_id = $hobbies_id;
+                $model->saveStrict();
+            }
+
+            return true;
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+}
